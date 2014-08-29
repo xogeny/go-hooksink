6c5569043a551f0016a5fe43c230fe5782a2e960
Michael Tiller <michael.tiller@gmail.com>
2014-08-29 15:38:17 -0400

    Got rid of some debugging output

--- testutils/post.go
+++ testutils/post.go
@@ -1,6 +1,5 @@
 package testutils
 
-import "log"
 import "bytes"
 import "net/http"
 import "net/http/httptest"
@@ -9,7 +8,6 @@ type HandleFunc func(res http.ResponseWriter, req *http.Request);
 
 func Post(f HandleFunc, path string, payload string) (status int, err error) {
 	raw := bytes.NewBuffer([]byte(payload));
-	log.Printf("POST Body: %v", raw);
 
 	req, err := http.NewRequest("POST", path, raw)
 	if (err!=nil) { return; }
