e534cebda011f9e0ab641f6e93188f2cb8f63c5e
Michael Tiller <michael.tiller@gmail.com>
2014-08-31 09:53:07 -0400

    Forgot to update this to conform to the latest interface definition

--- logger/hooklog.go
+++ logger/hooklog.go
@@ -4,14 +4,14 @@ import "log"
 
 import hs "github.com/xogeny/go-hooksink"
 
-type Logger struct {}
+type Logger struct{}
 
-func (l Logger) Push(msg hs.HubMessage) {
-	log.Printf("PUSH: %v", msg);
+func (l Logger) Push(msg hs.HubMessage, params map[string][]string) {
+	log.Printf("PUSH: %v", msg)
 }
 
 func main() {
-	h := hs.NewHookSink("ssshhhh!");
-	h.Add("/build", Logger{});
-	h.Start();
+	h := hs.NewHookSink("ssshhhh!")
+	h.Add("/build", Logger{})
+	h.Start()
 }
